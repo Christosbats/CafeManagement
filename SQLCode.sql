3cd68eca03a3877384a46bea5124427d2c15ed32
Christosbats <43824418+Christosbats@users.noreply.github.com>
2021-03-22 01:49:49 +0200

    Update SQLCode.sql

--- SQLCode.sql
+++ SQLCode.sql
@@ -7,17 +7,17 @@ category varchar(250) not null,
 price bigint not null);
 
 CREATE TABLE accounts(
-username varchar(250) not null,
+username varchar(250) not null primary key,
 password varchar(250) not null);
 
 CREATE TABLE SingleOrder(
-Item varchar(250) not null,
+Item varchar(250) not null primary key,
 UnitPrice bigint not null,
 Quantity int not null,
 TotalPrice bigint not null,
 ID smallint not null);
 
 CREATE TABLE TotalOrders(
-ID smallint not null,
+ID smallint not null primary key,
 TotalCost bigint not null);
 
